0569f165bac4b7be3b0310074fd0e4418ba61db9
doortje <doortje@wcw-stud-145-109-16-146.wireless.uva.nl>
2018-11-12 12:52:38 +0100

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by doortje on 12/11/2018.
+//  Copyright © 2018 Doortje. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
